d4e63b947ad7ec29c8e41837b35f5fa3ae42ea51
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-26 15:22:46 -0600

    Initial images and SVGs

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import "../styles/globals.css"
+
+import styled from "styled-components"
+
+const Header = styled.h2``
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <Header>hello world</Header>
+    </main>
+  )
+}
+
+export default IndexPage
